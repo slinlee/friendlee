3f5fd7f2d9a6039955b7a8e98c9b47b99187f7b4
Slin Lee <slinlee@gmail.com>
2013-07-30 15:43:38 -0700

    Sending indentify to mixpanel after a timeout to let the user data load.

--- client/friendlee.js
+++ client/friendlee.js
@@ -145,15 +145,17 @@ if (Meteor.isClient) {
 }
 
 jQuery(document).ready(function() {
-  if(Meteor.userId()) {
-    mixpanel.identify(Meteor.userId());
-    console.log("sending mixpanel identify");
-    mixpanel.name_tag(Meteor.user().profile.name);
-    mixpanel.people.set({
-        "Name" : Meteor.user().profile.name,
-        "$last_login": new Date()
-    });
-  }
+  setTimeout(function () {
+        if(Meteor.userId()) {
+            mixpanel.identify(Meteor.userId());
+            console.log("sending mixpanel identify");
+            mixpanel.name_tag(Meteor.user().profile.name);
+            mixpanel.people.set({
+                "Name" : Meteor.user().profile.name,
+                "$last_login": new Date()
+            });
+          }
+      }, 2000);
 });
 
 
