7afc76eb39ff83d96cd4a296494795f04891380a
Slin Lee <slinlee@gmail.com>
2013-12-26 20:15:25 -0800

    Adding a random prompt for the notes.

--- client/includes/new_note_modal.js
+++ client/includes/new_note_modal.js
@@ -8,15 +8,39 @@ Template.newNoteModal.rendered = function () {
     //Put input in to the dialog
     $('#newNoteModal').on('shown.bs.modal', function () {
         var input = $('#newNote');
+        input.val(Template.newNoteModal.random_prompt);
         var len = input.val().length;
         input[0].focus();
         input[0].setSelectionRange(len, len);
     })
 };
 
+Template.newNoteModal.random_prompt = function () {
+    var name = Template.newNoteModal.selected_name();
+    var promptArray = [
+        'One thing I appreciate about ' + name + ' is ',
+        'Three words I would use to describe ' + name + ' are ',
+        name + ' makes me smile when ',
+        'If ' + name + ' were a cereal, they would be ',
+        'If ' + name + ' was a fabric, they would be ',
+        name + ' is really good at ',
+        'I wish I could be more like ' + name + ' in this way '
+    ]
+
+    return promptArray[Math.floor(Math.random()*promptArray.length)];
+};
+
 Template.newNoteModal.events({
-    'click button.saveNote': function () {
-      Meteor.call('addFriendNote', Session.get("selected_friend"), Date.create('now'), $('#newNote').val());
+    'submit #newNoteModal form': function (e) {
+        e.preventDefault();
+
+        Meteor.call(
+            'addFriendNote',
+            Session.get("selected_friend"),
+            Date.create('now'),
+            $('#newNote').val()
+            );
+
       mixpanel.track("Added Note");
       $('#newNoteModal').modal('hide');
     }
