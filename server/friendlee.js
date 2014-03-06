79bc92764636d7ca0eec639d8469e02c86bb2165
Steve Chen <sebboh@gmail.com>
2014-03-06 12:24:43 -0800

    adds variable friendCard sizes based on number of dates met

--- server/friendlee.js
+++ server/friendlee.js
@@ -27,3 +27,15 @@ Meteor.publish("friend_notes", function() {
 Meteor.publish('friend_note_public', function(noteId) {
   return FriendNotes.find({_id: noteId});
 });
+
+Meteor.methods({
+  friendsAggregate: function() {
+    var pipe = [];
+    pipe.push( { $match: { owner: this.userId } } );
+    pipe.push( { $unwind: "$datesmet" } );
+    pipe.push( { $group: { _id: "$_id", size: { $sum:1 } } } );
+    pipe.push( { $sort: { size: 1 } } );
+    return Friends.aggregate(pipe);
+  }
+});
+
